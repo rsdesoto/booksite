cc71daf19d02a901e96ecf61f81e7542a23fc076
Ry <ry.e.desoto@gmail.com>
2019-01-22 16:24:11 -0600

    added sticky header to the books page

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-app.use("/view", express.static(path.join(__dirname, "/app/view")));
+app.use(express.static(path.join(__dirname, "/app/view")));
 
 require("./app/controller/apiroutes")(app);
 require("./app/controller/htmlroutes")(app);
